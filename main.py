1c6721de6dfb4dc78c01bb5dc4f0c1e5b37f4990
yegoryefimov <97941943+yegoryefimov@users.noreply.github.com>
2022-02-01 08:19:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+Rafael = turtle.Turtle()
+Rafael.color("green")
+Rafael.circle(100,steps=6)
+Rafael.forward(100)
+Rafael.color("blue")
+Rafael.circle(50)
+Rafael.backward(50)
+Rafael.color("red")
+Rafael.circle(50)
